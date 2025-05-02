31d96f63c7f032f0c43cceb4fcf0054a506b4b86
Esma-AlixianL <a.louraco@rns.ecolescreatives.com>
2025-05-02 15:55:49 +0200

    FelixMerge

---
+++ Assets/Scripts/GameManagement/GameManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public void ToggleTotalFreezePlayer()
+    {
+        PlayerBrain.Instance.playerCanMove = !PlayerBrain.Instance.playerCanMove;
+        PlayerBrain.Instance.playerCanLookAround = !PlayerBrain.Instance.playerCanLookAround;
+    }
+    
+    public void ToggleMovementFreezePlayer()
+    {
+        PlayerBrain.Instance.playerCanMove = !PlayerBrain.Instance.playerCanMove;
+    }
+    
+    public void ToggleCameraFreezePlayer()
+    {
+        PlayerBrain.Instance.playerCanLookAround = !PlayerBrain.Instance.playerCanLookAround;
+    }
+}
