54fe08bd197663444932434e6385b2b56a7bc6fc
Maxime <m.leroux@rns.ecolescreatives.com>
2025-05-02 13:42:27 +0200

    g

---
+++ Assets/Scripts/Enigmas/Tutorial/Dors.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class DoubleDoorInteraction : MonoBehaviour, IInteractable
+{
+    [Header("Interaction Settings"), Space(5)]
+    [SerializeField] private Animator[] _animators;
+    [SerializeField] private GameObject uiInteract;
+
+    public bool _isOpen = false;
+
+    public void Interact()
+    {
+        _isOpen = !_isOpen;
+        
+        foreach (var animator in _animators)
+        {
+            animator.SetTrigger("IsOpen");
+        }
+    }
+
+    public void ShowUI(bool show)
+    {
+        uiInteract.SetActive(show);
+    }
+}
