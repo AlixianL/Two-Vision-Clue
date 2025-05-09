80865b6b1a074143cbdb1e693f06d513dcd07d7f
CHAPELLIERE Youenn <y.chapelliere@rns.ecolescreatives.com>
2025-05-09 20:07:20 +0200

    [ UPDATE ] tablette infos

---
+++ Assets/1_Scripts/TabletesInfos/TabletteToggle.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class TabletteToggle : MonoBehaviour, IActivatable
+{
+    [Header("References"), Space(5)]
+    [SerializeField] private CanvasGroup _interactCanvasGroup;
+    [SerializeField] private CanvasGroup _informationsCanvasGroup; // L'élément à faire apparaître en fondu
+    [SerializeField] private Coroutine _informationsFadeCoroutine;
+    [SerializeField] private Coroutine _interactFadeCoroutine;
+    
+    [Header("Variables"), Space(5)]
+    [SerializeField] private float fadeDuration = 0.5f;
+    [SerializeField] private bool _infoIsOn;
+
+    
+    private void Start()
+    {
+        FadeOut();
+    }
+
+    public void Activate()
+    {   
+        if (_infoIsOn) FadeOut();
+        else FadeIn();
+    }
+    
+    public void FadeIn()
+    {
+        if (_informationsFadeCoroutine != null) StopCoroutine(_informationsFadeCoroutine);
+        _informationsFadeCoroutine = StartCoroutine(FadeCanvasGroup(_informationsCanvasGroup, 1f, fadeDuration));
+        _infoIsOn = true;
+        
+        if (_interactFadeCoroutine != null) StopCoroutine(_interactFadeCoroutine);
+        _interactFadeCoroutine = StartCoroutine(FadeCanvasGroup(_interactCanvasGroup, 0f, fadeDuration));
+    }
+
+    public void FadeOut()
+    {
+        if (_informationsFadeCoroutine != null) StopCoroutine(_informationsFadeCoroutine);
+        _informationsFadeCoroutine = StartCoroutine(FadeCanvasGroup(_informationsCanvasGroup, 0f, fadeDuration));
+        _infoIsOn = false;
+        
+        if (_interactFadeCoroutine != null) StopCoroutine(_interactFadeCoroutine);
+        _interactFadeCoroutine = StartCoroutine(FadeCanvasGroup(_interactCanvasGroup, 1f, fadeDuration));
+    }
+    
+    public IEnumerator FadeCanvasGroup(CanvasGroup canvasGroup, float targetAlpha, float duration)
+    {
+        float startAlpha = canvasGroup.alpha;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(startAlpha, targetAlpha, elapsed / duration);
+            yield return null;
+        }
+
+        canvasGroup.alpha = targetAlpha;
+        canvasGroup.interactable = (targetAlpha > 0.9f);
+        canvasGroup.blocksRaycasts = (targetAlpha > 0.9f);
+    }
+}
