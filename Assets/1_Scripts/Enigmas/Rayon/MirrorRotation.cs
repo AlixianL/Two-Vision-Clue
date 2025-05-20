75e7508d331a7380aeed4341d96962cafbf33775
felix.hautin <felix.hautin@hotmail.fr>
2025-05-20 16:48:09 +0200

    enigme final fonctionnel

--- Assets/1_Scripts/Enigmas/Rayon/MirrorRotation.cs
+++ Assets/1_Scripts/Enigmas/Rayon/MirrorRotation.cs
@@ -34,7 +34,12 @@ public class MirrorRotation : MonoBehaviour, IActivatable
 
         GameManager.Instance.ToggleTotalFreezePlayer();
         PlayerBrain.Instance.playerRigidbody.linearVelocity = Vector3.zero;
-        ChangePositionCinemachine.Instance.SwitchCam(_enigmaCinemachineCamera, _interactWithEnigma);
+
+        if (_enigmaCinemachineCamera != null)
+        {
+            ChangePositionCinemachine.Instance.SwitchCam(_enigmaCinemachineCamera, _interactWithEnigma);
+        }
+        
         
         GameManager.Instance.playerUI.SetActive(!_interactWithEnigma);
         GameManager.Instance.mirrorUI.SetActive(_interactWithEnigma);
