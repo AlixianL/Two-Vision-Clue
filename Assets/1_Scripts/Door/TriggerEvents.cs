53f18c926a2c75a151dd0ec185045aa07350dc0e
CHAPELLIERE Youenn <y.chapelliere@rns.ecolescreatives.com>
2025-05-28 18:39:56 +0200

    Préparation au merge

--- Assets/1_Scripts/Door/TriggerEvents.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class TriggerEvents : MonoBehaviour
-{
-    public void SwitchCameraOnPlayer()
-    {
-        ChangePositionCinemachine.Instance.ReturnOnPlayerCinemachineCamera(ChangePositionCinemachine.Instance._doorCinemachineCamera);
-    }
-}
